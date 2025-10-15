c00ba14c28be54af542e9171539193ecbe336190
essafi.wijdane49@gmail.com <wijdanee76@gmail.com>
2025-10-15 15:50:50 +0200

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -18,8 +18,8 @@ CREATE TABLE IF NOT EXISTS Books (
 
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
     address TEXT
 );
 
